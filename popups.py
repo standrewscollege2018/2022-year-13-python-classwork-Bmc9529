b20788b78ce828e7b6ed7fe0ae0420394619638e
Bmc9529 <bmc9529@stacmail.net>
2022-07-06 11:58:07 +1200

    popups

--- popups.py
+++ popups.py
@@ -6,9 +6,6 @@ class Item():
         self._value = value
 
         all_items.append(self)
-        #appened name to item_names list
-        item_names.append(name)
-
    
     def get_name(self):
         ''' Return name of item '''
@@ -36,57 +33,50 @@ def generate_items():
             
             Item(line[0], int(line[1]))
 
-# function to print value
-def print_value(v):
-
-    print(v)
-    item_value.set(f"${v}")
-
-def display_selection():
-    '''Get the selected item from the option menu and display details in a label'''
-
-    #loop through all items until we find the one we selected, ad get the value
-    for i in all_items:
-        if i.get_name() == selected_item.get():
-            value= i.get_value()
-    details.set(f"{selected_item.get()} costs ${value}")
+def print_selection():
+    '''print out selected items'''
+    for num in listbox.curselection():
+        something = True
+        while something == True:
+            messagebox.showinfo(f"{all_items[num].get_name()}", f"{all_items[num].get_name()} costs ${all_items[num].get_value()}")
         
+
     
 # Import tkinter
 from tkinter import *
 from functools import partial
+from tkinter import messagebox
 
 # List to store all items
 all_items = []
-#list of all item names so we can populate the option menu
-item_names = ["Select Names"]
 
 #import the items from csv file
 generate_items()
 
-#setup GUI
+#########   SET UP GUI #########
 from tkinter import *
 root = Tk()
-root.title("Option menu demo")
+root.title("Popups demo")
 root.geometry("800x500")
 
-#OPtion menu
-#set up a variable to store the selection
-selected_item = StringVar()
-selected_item.set(item_names[0])
-names_menu = OptionMenu(root, selected_item, *item_names)
-names_menu.config(fg="red")
-names_menu.grid(row=0)
+listbox = Listbox(root, selectmode=SINGLE)
+listbox.grid(row=0)
 
+#Populate listbox with names of items
+for i in all_items:
+    listbox.insert(END, i.get_name())
 
-'''ADd a button that gets the selected item, then displays the price in a label'''
-#button
-select_btn = Button(root, text="Select", command=display_selection)
-select_btn.grid(row=0, column=1)
-#Label
+#button to enter selection
+select_btn = Button(root, text="selection", command=print_selection)
+select_btn.grid(row=1)
+
 details = StringVar()
 details_lbl = Label(root, textvariable=details)
 details_lbl.grid(row=1, column=1)
+
+
+
+
 
 
 #launch the program
